503a53e3daa7418d99a289bbd9d9d67383ec3d3a
Yuriy Kozhin <YKozhin@IBS.RU>
2022-05-20 16:30:53 +0300

    change example

--- 01-rbac-example/1.sh
+++ 01-rbac-example/1.sh
@@ -1,13 +1,72 @@
 #!/bin/bash
 
-openssl genrsa -out petr.key 4096
-openssl req -config csr.cnf -new -key petr.key -nodes -out petr.csr
-export BASE64_CSR=$(cat petr.csr | base64 | tr -d '\n')
-cat csr.yaml | envsubst | kubectl apply -f -
-kubectl create ns rbac-test
-kubectl get csr
-kubectl certificate approve petr_csr
-kubectl get csr petr_csr -o jsonpath={.status.certificate} | base64 --decode > petr.crt
-kubectl config --kubeconfig=./config set-cluster k8s --server=http://192.168.0.101:6443 --certificate-authority=/etc/kubernetes/pki/ca.crt --embed-certs=true
-kubectl config --kubeconfig=./config set-credentials petr --client-key=petr.key --client-certificate=petr.crt --embed-certs=true
-kubectl config --kubeconfig=./config set-context default --cluster=k8s --user=auth --namespace rbac-test
+export ACCOUNT_NAME=petr
+export NAMESPACE=rbac-test
+export ROLENAME=petr-example-role
+
+kubectl create ns $NAMESPACE
+kubectl create serviceaccount $ACCOUNT_NAME --namespace $NAMESPACE
+
+TOKEN_NAME=$(kubectl get serviceAccounts $ACCOUNT_NAME --namespace $NAMESPACE  -o jsonpath="{.secrets[0].name}")
+TOKEN=$(kubectl describe secrets $TOKEN_NAME --namespace $NAMESPACE | grep 'token:' | rev | cut -d ' ' -f1 | rev)
+CERTIFICATE_AUTHORITY_DATA=$(kubectl config view --flatten --minify -o jsonpath="{.clusters[0].cluster.certificate-authority-data}")
+SERVER_URL=$(kubectl config view --flatten --minify -o jsonpath="{.clusters[0].cluster.server}")
+CLUSTER_NAME=$(kubectl config view --flatten --minify -o jsonpath="{.clusters[0].name}")
+
+cat <<EOF > $CLUSTER_NAME-$ACCOUNT_NAME-kube.conf
+apiVersion: v1
+kind: Config
+users:
+- name: $ACCOUNT_NAME
+  user:
+    token: $TOKEN
+clusters:
+- cluster:
+    certificate-authority-data: $CERTIFICATE_AUTHORITY_DATA
+    server: $SERVER_URL
+  name: $CLUSTER_NAME
+contexts:
+- context:
+    cluster: $CLUSTER_NAME
+    user: $ACCOUNT_NAME
+  name: $CLUSTER_NAME-$ACCOUNT_NAME-context
+current-context: $CLUSTER_NAME-$ACCOUNT_NAME-context
+EOF
+
+kubectl --kubeconfig=$CLUSTER_NAME-$ACCOUNT_NAME-kube.conf get po -n $NAMESPACE
+
+cat <<EOF > $ROLENAME-role.yaml ; kubectl apply -f $ROLENAME-role.yaml -n $NAMESPACE
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  namespace: $NAMESPACE
+  name: $ROLENAME
+rules:
+- apiGroups: [""]
+  resources: ["pods", "pods/log", "services", "persistentvolumeclaims"]
+  verbs: ["get", "list", "watch", "describe"]
+- apiGroups: [""]
+  resources: ["pods/exec"]
+  verbs: ["create"]
+- apiGroups: ["extensions"]
+  resources: ["ingresses"]
+  verbs: ["get", "list", "watch"]
+EOF
+
+cat <<EOF > $ROLENAME-rolebinding.yaml ; kubectl apply -f $ROLENAME-rolebinding.yaml -n $NAMESPACE
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: $ACCOUNT_NAME-$ROLENAME-rolebinding
+  namespace: $NAMESPACE
+subjects:
+- kind: User
+  name: system:serviceaccount:$NAMESPACE:$ACCOUNT_NAME # Name is case sensitive
+  apiGroup: rbac.authorization.k8s.io
+roleRef:
+  kind: Role #this must be Role or ClusterRole
+  name: $ROLENAME # this must match the name of the Role or ClusterRole you wish to bind to
+  apiGroup: rbac.authorization.k8s.io
+EOF
+
+kubectl --kubeconfig=$CLUSTER_NAME-$ACCOUNT_NAME-kube.conf get po -n $NAMESPACE
