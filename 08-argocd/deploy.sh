68bfba45658db45ec1fcfc27c1b3223f15a28432
Yuriy Kozhin <YKozhin@IBS.RU>
2024-02-04 12:08:36 +0300

    add command for ArgoCD initial admin password getting

--- 08-argocd/deploy.sh
+++ 08-argocd/deploy.sh
@@ -4,6 +4,5 @@ version=2.9.6
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v${version}/manifests/install.yaml
-
 kubectl apply -f argocd-ingress.yml
-
+kubectl get secret  argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d ; echo
